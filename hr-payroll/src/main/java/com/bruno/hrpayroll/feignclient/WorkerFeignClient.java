d8d611be730c335594c0955e371c9f2056b76043
brunovicentealves <brunovicentealves.ti@gmail.com>
2021-05-18 22:34:29 -0300

    FeignClient

---
+++ hr-payroll/src/main/java/com/bruno/hrpayroll/feignclient/WorkerFeignClient.java
@@ -0,0 +1,17 @@
+package com.bruno.hrpayroll.feignclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.bruno.hrpayroll.entities.Worker;
+
+@Component
+@FeignClient(name = "ht-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+	@GetMapping("/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
