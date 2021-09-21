aa8da14e0e738625ecee2bdd71bd20c2197af3d0
brunovicentealves <brunovicentealves.ti@gmail.com>
2021-09-21 20:40:49 -0300

    Actuator for Runtime COnfig refresh

--- hr-worker/src/main/java/com/bruno/hrworker/resource/WorkeResource.java
+++ hr-worker/src/main/java/com/bruno/hrworker/resource/WorkeResource.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bruno.hrworker.entities.Worker;
 import com.bruno.hrworker.repostitory.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 public class WorkeResource {
